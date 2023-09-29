8ee48168c4a5cf8b94312760af528224a942469c
Titas <titas@pixylt.com>
2023-09-29 14:12:49 +0300

    update

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import Post from './Post';
+import axios from 'axios';
 
 function List() {
+  // https://api.publicapis.org/random
+
+  axios.get('https://api.publicapis.org/random')
+    .then(reponse => {
+      console.log(reponse.data);
+    });
+
   const posts = [
     {
       'id': 1,
